02eaba2dbbc8419bcac0ec5339082019d4eb46b0
chocovish <vishal.ghosh28@gmail.com>
2019-02-11 17:43:10 +0530

    initial

--- eventbooking/settings.py
+++ eventbooking/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6_50uib&^m(!6t4d@6fb7nw#r$5)hgffyn&ilivaw95)aroncv'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'mainapp.User'
